ba17b16b33065069d49f3bd41b6173faa0cdf127
Mohammad Ansil :) <146548918+MasterOfBrokenLogic@users.noreply.github.com>
2026-03-29 22:58:14 +0530

    Add EvenOddCLA class to check even or odd numbers

---
+++ EvenOddCLA.java
@@ -0,0 +1,12 @@
+public class EvenOddCLA {
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+
+        if(n % 2 == 0) {
+            System.out.println("Number is Even");
+        } else {
+            System.out.println("Number is Odd");
+        }
+    }
+}
