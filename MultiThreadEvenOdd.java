8c3524ba45e6388b91a0744f55139e0c5e67721e
Mohammad Ansil :) <146548918+MasterOfBrokenLogic@users.noreply.github.com>
2026-03-29 23:01:37 +0530

    Update MultiThreadEvenOdd.java

--- MultiThreadEvenOdd.java
+++ MultiThreadEvenOdd.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author teams
- */
 class EvenThread extends Thread {
     public void run() {
         for(int i = 2; i <= 10; i += 2) {
